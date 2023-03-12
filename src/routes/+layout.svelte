ff96fb826aa39ca96b343d5746dafa2365adb7ba
Simen <zimen2004@gmail.com>
2023-03-12 15:19:14 +0100

    Optimized global css loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
     import Navbar from "$lib/components/navbar.svelte"
-
-    import "$lib/app.css"
 </script>
 
 <Navbar />
@@ -10,11 +8,6 @@
 </main>
 
 <style>
-    :global(body){
-        margin: 0;
-        background-color: #2B2D42;
-        color: #EDF2F4;
-    }
     main{
         margin: 15px 30px 15px 30px;
     }
