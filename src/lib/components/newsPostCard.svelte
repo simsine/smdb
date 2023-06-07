85b392762ef81fadee940ca33cc7d8f199050402
Simen <zimen2004@gmail.com>
2023-06-07 22:38:35 +0200

    Date formatting

--- src/lib/components/newsPostCard.svelte
+++ src/lib/components/newsPostCard.svelte
@@ -3,21 +3,27 @@
 
 	import type { PageData } from "./$types"
 	export let newspost: PageData
+
+	let date = new Date(newspost.date)
 </script>
 
 <article>
 	<h2>{newspost.title}</h2>
 	<SvelteMarkdown source={newspost.markdown} />
-	<p>{newspost.date}</p>
+	<section>
+		<i>{date.getDate()}.{date.getMonth()}.{date.getFullYear()}</i>
+	</section>
 </article>
 
 <style>
 	article {
-		/* outline: auto; */
 		margin: 3rem 0;
 		padding: 3rem 1.5rem;
-		background-color: hwb(235 17% 76% / 1);
+		background-color: rgb(43, 45, 61);
 		box-shadow: var(--box-shadow);
 		border-radius: 0.25rem;
+	}
+	section {
+		text-align: end;
 	}
 </style>
