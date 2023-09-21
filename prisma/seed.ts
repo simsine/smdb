4dbbcdb5d388a953edb263bb69312877a136040e
Simen Strømsnes <676624@stud.hvl.no>
2023-09-21 13:13:54 +0200

    updated db seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -25,7 +25,25 @@ async function main() {
 			},
 		},
 	})
-	console.log(bjarne)
+
+	// const bjarne_profile = await pc.userProfile.upsert({
+	// 	where:{userId: bjarne.id},
+	// 	update:{},
+	// 	create:{
+
+	// 	}
+	// })
+
+	const newspost = await pc.news.upsert({
+		where: { id: 1 },
+		update: {},
+		create: {
+			title: "Developers, developers, developers...",
+			markdown:
+				"Hi, this is a post that has been seeded to the development database, if you see this then you have most likely done something right!",
+		},
+	})
+	console.log(bjarne, newspost)
 }
 main()
 	.then(async () => {
