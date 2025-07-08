6f17c6b0bf2c7faef3453c567b3f5e303cf96ef5
Simen Strømsnes <simen@simsine.no>
2025-07-09 01:14:59 +0200

    Lool

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,8 +1,8 @@
-import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+import type { Config } from '@sveltejs/kit';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: Config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
