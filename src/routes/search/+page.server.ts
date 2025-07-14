75c04a1cca87c6db7aa8c82788df8702803e75e3
Simen Strømsnes <simen@simsine.no>
2025-07-14 14:45:47 +0200

    Add trim to search

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -2,7 +2,7 @@ import { SECRET_APIKEY } from "$env/static/private"
 
 export const load = (async ({ fetch, url }) => {
 	let searchFilter = url.searchParams.get("f")
-	let searchQuery = url.searchParams.get("s")
+	let searchQuery = url.searchParams.get("s")?.trim()
 	let searchPage = url.searchParams.get("p") || "1"
 
 	let response = await fetch(`https://www.omdbapi.com/?apikey=${SECRET_APIKEY}&s=${searchQuery}&type=${searchFilter}&page=${searchPage}`)
